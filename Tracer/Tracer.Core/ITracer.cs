2ccd4f17c409279021642dd8dd810acb05f77c0d
NotDmitry <91372817+NotDmitry@users.noreply.github.com>
2022-11-07 22:27:44 +0300

    File restructuring and new feature
    Deleted:
    - Template for console apllication: file Program.cs.
    
    Added:
    - Declaration for Tracer and TraceResult classes;
    - Interface Description for ITracer.

---
+++ Tracer/Tracer.Core/ITracer.cs
@@ -0,0 +1,14 @@
+﻿namespace Tracer.Core
+{
+    public interface ITracer
+    {
+        // Call at start
+        void StartTrace();
+
+        // Call at the end
+        void StopTrace();
+        
+        // Shoud return measurement results
+        TraceResult GetTraceResult();
+    }
+}
