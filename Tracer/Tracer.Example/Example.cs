6e556711dfe27a0a76300b20e369b9ec5db388e0
NotDmitry <91372817+NotDmitry@users.noreply.github.com>
2022-11-12 02:29:52 +0300

    Release update
    New test example

--- Tracer/Tracer.Example/Example.cs
+++ Tracer/Tracer.Example/Example.cs
@@ -4,7 +4,21 @@ using Tracer.Serialization.Abstractions;
 
 var tracer = new Tracer.Core.Tracer();
 var foo = new Foo(tracer);
-foo.MyMethod();
+var bar = new Bar(tracer);
+var c = new C(tracer);
+
+
+var thread1 = new Thread(foo.MyMethod);
+var thread2 = new Thread(bar.InnerMethod);
+var thread3 = new Thread(c.M0);
+
+thread1.Start();
+thread2.Start();
+thread3.Start();
+
+thread1.Join();
+thread2.Join();
+thread3.Join();
 
 var traceResult = tracer.GetTraceResult();
 
@@ -26,8 +40,9 @@ public class Foo
     public void MyMethod()
     {
         _tracer.StartTrace();
-        Thread.Sleep(100);
+        Thread.Sleep(200);
         _bar.InnerMethod();
+        Thread.Sleep(50);
         _tracer.StopTrace();
     }
 }
@@ -42,6 +57,36 @@ public class Bar
     }
 
     public void InnerMethod()
+    {
+        _tracer.StartTrace();
+        Thread.Sleep(200);
+        _tracer.StopTrace();
+    }
+}
+
+public class C
+{
+    private ITracer _tracer;
+
+    public C(ITracer tracer)
+    {
+        _tracer = tracer;
+    }
+
+    public void M0()
+    {
+        M1();
+        M2();
+    }
+
+    private void M1()
+    {
+        _tracer.StartTrace();
+        Thread.Sleep(100);
+        _tracer.StopTrace();
+    }
+
+    private void M2()
     {
         _tracer.StartTrace();
         Thread.Sleep(200);
