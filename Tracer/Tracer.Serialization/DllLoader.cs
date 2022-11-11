12468482a173f1ae6a5b16b702623794ba2f467a
NotDmitry <91372817+NotDmitry@users.noreply.github.com>
2022-11-11 21:16:05 +0300

    New feature
    Added:
    - Class with static methods for loading plugins
    and printing serialization results to file

---
+++ Tracer/Tracer.Serialization/DllLoader.cs
@@ -0,0 +1,55 @@
+﻿using System.Reflection;
+using Tracer.Core;
+using Tracer.Serialization.Abstractions;
+
+namespace Tracer.Serialization;
+
+public class DllLoader
+{
+
+    private static IEnumerable<Type> GetAllTypesThatImplementInterface<T>(Assembly assembly)
+    {
+        return assembly.GetTypes().Where(type => typeof(T).IsAssignableFrom(type) 
+        && !type.IsInterface);
+    }
+
+    public static IEnumerable<T> LoadPlugins<T>(string path)
+    {
+
+        var pluginsList = new List<T>();
+
+        DirectoryInfo searchDirectory = new(path);
+        if (!searchDirectory.Exists)
+            return pluginsList;
+
+        FileInfo[] files = searchDirectory.GetFiles("*.dll");
+        foreach (FileInfo file in files)
+        {
+
+            var assembly = Assembly.LoadFrom(file.FullName);
+            foreach (var type in GetAllTypesThatImplementInterface<T>(assembly))
+            {
+                var pluginInstance = Activator.CreateInstance(type);
+                if (pluginInstance is not null)
+                    pluginsList.Add((T)pluginInstance);
+            }
+
+        }
+
+        return pluginsList;
+
+    }
+
+    public static void PrintSerializeResult(IEnumerable<ITraceResultSerializer> pluginList, 
+        TraceResult traceResult)
+    {
+        foreach (var plugin in pluginList)
+        {
+            var i = 1;
+            using (FileStream fstream = new FileStream($"{i++}.txt", FileMode.Create))
+            {
+                plugin.Serialize(traceResult, fstream);
+            }
+        }
+    }
+}
