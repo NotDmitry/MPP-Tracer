d5ad26700f346764e079733b704ad4631ec4ab1e
NotDmitry <91372817+NotDmitry@users.noreply.github.com>
2022-11-11 18:49:54 +0300

    New feature
    Added:
    - Interface ITraceResultSerializer

--- Tracer/Tracer.Core/Tracer.cs
+++ Tracer/Tracer.Core/Tracer.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Concurrent;
-using System.Collections.Generic;
+﻿using System.Collections.Concurrent;
 using System.Diagnostics;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Tracer.Core;
 
